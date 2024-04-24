db7b0b859360c9cf5dfd54985ba9ace644abab1e
root <root@SURFACE.>
2024-04-24 09:45:57 +0300

    blur

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function FocusBlurButton() {
+    // Handler for the focus event
+    const handleFocus = () => {
+        console.log('Good!');
+    };
+
+    // Handler for the blur event
+    const handleBlur = () => {
+        console.log('Hey! Eyes on me!');
+    };
+
+    return (
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>
+                Eyes on me
+            </button>
+        </div>
+    );
+}
+
+export default FocusBlurButton;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange() {
+        console.log("Entering password...")
+
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" placeholder="Enter password" onChange={handleChange}/>
+        </div>
     )
 }
 
